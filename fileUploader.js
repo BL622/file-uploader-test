433d62bdf699b7ecd19ee2d70140fb95dabc6a18
BL622 <baloghm.levente@gmail.com>
2024-06-09 01:43:26 +0200

    modified fileuploader class now only crops image if needed, and writing to folder works

--- fileUploader.js
+++ fileUploader.js
@@ -1,126 +1,126 @@
 const sharp = require('sharp');
+const { log } = require('./log');
 
 class FileUploader {
     constructor(options) {
         this.options = options;
     }
 
-    log(message) {
-        console.log(message);
-    }
-
     async validateFileType(file, allowedTypes) {
-        this.log(`Validating file type: ${file.mimetype}`);
+        log(2, `Validating file type: ${file.mimetype}`);
         if (!allowedTypes.includes(file.mimetype)) {
-            throw new Error(`Invalid file type: ${file.mimetype}`);
+            return { error: `Invalid file type: ${file.mimetype}` };
         }
-        this.log('File type validated successfully');
+        log(2, 'File type validated successfully');
+        return true
     }
 
     async validateFileSize(file, options) {
-        this.log(`Validating file size: ${file.size}`);
+        log(2, `Validating file size: ${file.size}`);
         if (file.size > options.maxSize) {
-            throw new Error(`File size exceeds the maximum limit of ${options.maxSize} bytes`);
+            return { error: `File size exceeds the maximum limit of ${options.maxSize} bytes` };
         }
-        this.log('File size validated successfully');
+        log(2, 'File size validated successfully');
+        return true
     }
 
     async validateImage(file, options) {
-        this.log('Validating image dimensions');
+        log(2, 'Validating image dimensions');
         try {
             const metadata = await sharp(file.buffer).metadata();
-            if (metadata.width > options.maxWidth || metadata.height > options.maxHeight) {
+            if (options.cropImage && (metadata.width > options.maxWidth || metadata.height > options.maxHeight)) {
                 const targetSize = Math.min(options.maxWidth, options.maxHeight);
                 const sharpInstance = sharp(file.buffer).resize({
                     width: targetSize,
                     height: targetSize,
                     fit: 'cover'
                 });
-                if (options.cropImage) {
-                    await sharpInstance.toBuffer()
-                        .then((resizedBuffer) => {
-                            file.buffer = resizedBuffer;
-                        });
-                    this.log('Image cropped to 1:1 aspect ratio');
-                } else {
-                    await sharpInstance.toBuffer()
-                        .then((resizedBuffer) => {
-                            file.buffer = resizedBuffer;
-                        });
-                    this.log('Image resized to fit dimensions');
-                }
+                const croppedBuffer = await sharpInstance.toBuffer();
+                file.buffer = croppedBuffer;
+                log(2, 'Image cropped to 1:1 aspect ratio');
+            } else if (!options.cropImage && (metadata.width > options.maxWidth || metadata.height > options.maxHeight)) {
+                return { error: 'Image dimensions exceed maximum allowed dimensions. CropImage option is false. Image cannot be uploaded.' };
+            } else {
+                log(2, 'Image dimensions within maximum allowed dimensions. No need for cropping');
             }
-            this.log('Image dimensions validated successfully');
+            log(2, 'Image dimensions validated successfully');
+            return true;
         } catch (error) {
-            throw new Error(`Invalid image file: ${error.message}`);
+            return { error: `Invalid image file: ${error.message}` };
         }
     }
 
+
+
+
     async validateAudio(file, options) {
-        this.log('Validating audio duration and format');
+        log(2, 'Validating audio duration and format');
         try {
             const mm = await import('music-metadata');
             const metadata = await mm.parseBuffer(file.buffer, { duration: true });
             const duration = metadata.format.duration;
-            if (duration > options.maxDuration) {
-                throw new Error('Audio duration is too long');
-            }
-            this.log('Audio duration and format validated successfully');
+            if (duration > options.maxDuration) return { error: 'Audio duration is too long' };
+            log(2, 'Audio duration and format validated successfully');
             return true;
         } catch (error) {
-            throw new Error(`Invalid audio file: ${error.message}`);
+            return { error: `Invalid audio file: ${error.message}` };
         }
     }
 
     async validate(filesToValidate) {
-        try {
-            let hasAlbumCover = false;
-            let hasAlbumSongs = false;
+        let validationErrors = [];
     
-            for (const fieldName in filesToValidate) {
-                const files = filesToValidate[fieldName];
-                const fieldOptions = this.options.fields.find(field => field.name === fieldName);
+        const requiresSongs = this.options.fields.some(field => field.type === 'audio' && field.album);
     
-                if (!fieldOptions) {
-                    throw new Error(`Options not found for field: ${fieldName}`);
+        for (const fieldName in filesToValidate) {
+            const files = filesToValidate[fieldName];
+            const fieldOptions = this.options.fields.find(field => field.name === fieldName);
+    
+            if (!fieldOptions) {
+                validationErrors.push({ error: `Options not found for field: ${fieldName}` });
+                continue;
+            }
+    
+            for (const file of files) {
+                log(2, `Validating ${fieldName}`);
+                const fileTypeValidation = await this.validateFileType(file, fieldOptions.allowedTypes);
+                if (fileTypeValidation.error) {
+                    validationErrors.push(fileTypeValidation);
+                    continue;
                 }
     
-                for (const file of files) {
-                    this.log(`Validating ${fieldName}`);
-                    try {
-                        await this.validateFileType(file, fieldOptions.allowedTypes);
-                        await this.validateFileSize(file, fieldOptions);
+                const fileSizeValidation = await this.validateFileSize(file, fieldOptions);
+                if (fileSizeValidation.error) {
+                    validationErrors.push(fileSizeValidation);
+                    continue;
+                }
     
-                        if (fieldOptions.type === 'image' && fieldOptions.name === 'albumCover') {
-                            await this.validateImage(file, fieldOptions);
-                            hasAlbumCover = true;
-                        } else if (fieldOptions.type === 'audio' && fieldOptions.name === 'songs') {
-                            await this.validateAudio(file, fieldOptions);
-                            hasAlbumSongs = true;
-                        }
-                        this.log(`${fieldName} validated successfully`);
-                    } catch (error) {
-                        this.log(`Validation failed for ${fieldName}: ${error.message}`);
-                        throw error;
+                if (fieldOptions.type === 'image') {
+                    const imageValidation = await this.validateImage(file, fieldOptions);
+                    if (imageValidation.error) {
+                        validationErrors.push(imageValidation);
+                    }
+                } else if (fieldOptions.type === 'audio') {
+                    const audioValidation = await this.validateAudio(file, fieldOptions);
+                    if (audioValidation.error) {
+                        validationErrors.push(audioValidation);
                     }
                 }
+                log(2, `${fieldName} validated successfully`);
             }
-    
-            if (hasAlbumCover && !hasAlbumSongs) {
-                throw new Error('At least one song must be uploaded for the album');
-            }
-    
-            this.log('All files validated successfully');
-            return true;
-        } catch (error) {
-            this.log(`Validation failed: ${error.message}`);
-            throw error;
         }
+    
+        if (requiresSongs && !filesToValidate['songs']) {
+            validationErrors.push({ error: 'At least one song must be uploaded for the album' });
+        }
+    
+        if (validationErrors.length > 0) {
+            return validationErrors;
+        }
+    
+        log(2, 'All files validated successfully');
+        return true;
     }
-    
-    
-
-
 }
 
 module.exports = FileUploader;
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const cors = require('cors')
 const { Request } = require('./utils.js')
 const { log } = require('./log.js')
 const mongoose = require('mongoose')
-const { userModel } = require('./models.js')
 const fs = require('fs')
 const path = require('path')
 
@@ -11,7 +10,6 @@ const path = require('path')
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.jjsuomx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/test`)
     .then(() => {
         log(1, "Successfully connected to DB.")
-        // userModel.create({username: 'gipsz_jakab', email: 'gipsz_jakab@teszt.com', password: 'teszt123'})
     }
     )
     .catch(err =>
@@ -25,146 +23,92 @@ app.use(express.urlencoded({ extended: true }))
 app.listen(process.env.SERVER_PORT);
 log(1, `Server stated on port ${process.env.SERVER_PORT}`)
 
-app.post('/api/login', (req, res) => postLogin(req, res))
-app.post('/api/register', (req, res) => postRegister(req, res))
-
-app.get('/api/user/:username', (req, res) => getUser(req, res))
 
 
-
-
+//validation and uploading
 const multer = require('multer');
 const FileUploader = require('./fileUploader.js');
 const upload = multer({ storage: multer.memoryStorage() });
-app.post('/api/user/:username/editProfile', upload.single('profile'), (req, res) => postProfileImg(req, res))
 
-app.get('/api/image/defaults/:imageName', (req, res) => getImageDefaults(req, res))
-
-async function postLogin(req, res) {
+app.post('/api/user/:username/editProfile', upload.single('profile'), async (req, res) => {
     const request = new Request(req, res)
-    const post = request.validatePostFields(["username", "password"])
-    if (post === false) return request.respond400MissingPostFields()
 
-
-    let user = await userModel.findOne({ username: post.username }).exec()
-    if (user === null || !(await request.compareHash(post.password, user.password))) return request.respond400Error("Invalid username or password")
-
-    const token = await request.generateToken()
-    userModel.findOneAndUpdate({ _id: user._id }, { token: token }).exec()
-
-    request.respond200Success({ token: token, username: user.username })
-}
-
-async function postRegister(req, res) {
-    const request = new Request(req, res)
-    const post = request.validatePostFields(["username", "email", "password"])
-    if (post === false) return request.respond400MissingPostFields()
-
-    const password = await request.passwordHash(post.password)
-    const token = await request.generateToken()
-    userModel.create({ username: post.username, password: password, token: token, email: post.email })
-        .then(() => {
-            request.respond200Success({ token: token, username: post.username })
-        })
-        .catch(err => {
-            if (err.errorResponse.code == 11000) {
-                const duplicate_field = Object.keys(err.errorResponse.keyPattern)[0]
-                if (duplicate_field == "username") request.respond400Error('Username already in use.')
-                else if (duplicate_field == "email") request.respond400Error('Email already in use.')
-                else log(1, err)
-            }
-            else log(1, err)
-        })
-
-}
-
-async function getUser(req, res) {
-    const request = new Request(req, res)
-    const username = request.params['username']
-
-    const userDetails = await userModel.findOne({ username: username }).select('username profile_img is_artist artist_name -_id').exec()
-    request.respond200Success(userDetails)
-}
-
-async function getImageDefaults(req, res) {
-    const request = new Request(req, res)
-    const imagePath = `${__dirname}/images/defaults/${request.params.imageName}`
-
-    if (!fs.existsSync(imagePath)) return request.respond404NotFound()
-    request.respond200File(imagePath)
-}
-
-
-const profileImageUploader = new FileUploader({
-    fields: [
-        { name: 'profile', type: 'image', allowedTypes: ['image/jpeg', 'image/png'], maxWidth: 1024, maxHeight: 1024, maxSize: 5 * 1024 * 1024, cropImage: true } // 5 MB
-    ]
-});
-
-async function postProfileImg(req, res) {
-
+    const fileExtension = path.extname(req.file.originalname);
+    const outputPath = `${__dirname}/images/profiles/${request.params.username.toLowerCase()}${fileExtension}`;
+    const existingImagePath = `${__dirname}/images/profiles/${request.params.username.toLowerCase()}${fileExtension}`;
     const filesToValidate = {
         profile: [req.file]
     };
 
-    try {
-        await profileImageUploader.validate(filesToValidate);
-        res.json({ message: 'Profile image uploaded successfully' });
-    } catch (error) {
-        console.error('Error uploading profile image:', error);
-        res.status(400).send(error.message);
-    }
-    // const request = new Request(req, res)
+    const validation = await profileImageUploader.validate(filesToValidate);
+    if (Array.isArray(validation) && validation.length > 0) return request.respond400Error(validation[0].error)
 
-    // console.log(req.file)
-    // // const outputPath = `${__dirname}/images/profiles/`
-
-    // const post = request.validatePostFields(['token', 'username'], ['artist_name'])
-    // if (post === false) return request.respond400MissingPostFields()
-
-    // request.handleUpload(profilePictureUploaderOptions, () => {
-    //     request.respond200Success({ message: 'Profile picture uploaded successfully!' });
-    // });
-
-    // const imagePath = `/images/profiles/${req.params.username.toLowerCase() + '.' + req.file.mimetype.split('/')[1]}`
-    // const options = {
-    //     profile: { type: 'image', width: 1024, height: 1024, outputPath }
-    // }
-    // const results = await request.validatePostFiles(options)
-    // if (results.error) return request.respond400Error(results.error)
-
-    // const query = {token: post.token}
-    // let values = {$set: {profile_img: imagePath, username: req.params.username, is_artist: false, artist_name: post.artist_name}}
-    // if(post.artist_name !== "") values.$set = {...values.$set, is_artist: true, artist_name: post.artist_name}
-
-    // await userModel.updateOne(query, values).then(()=>{
-    //     request.respond200File(__dirname + imagePath)
-    // }).catch((err => {
-    //     if(!err) log(0, err)
-    // }))
-}
+    if (fs.existsSync(existingImagePath)) fs.unlinkSync(existingImagePath)
+        fs.writeFileSync(outputPath, req.file.buffer);
 
 
-app.post('/api/upload/music-album', upload.fields([{ name: 'albumCover', maxCount: 1 }, { name: 'songs', maxCount: 10 }]), (req, res) => postAlbums(req, res))
+    request.respond200File(outputPath)
+});
 
-const albumUploader = new FileUploader({
+const profileImageUploader = new FileUploader({
     fields: [
-        { name: 'albumCover', type: 'image', allowedTypes: ['image/jpeg', 'image/png'], maxWidth: 1024, maxHeight: 1024, maxSize: 5 * 1024 * 1024 }, // 5 MB
-        { name: 'songs', type: 'audio', allowedTypes: ['audio/mpeg', 'audio/wav'], maxDuration: 300, maxSize: 50 * 1024 * 1024 } // 50 MB
+        { name: 'profile', type: 'image', allowedTypes: ['image/jpeg', 'image/png'], maxWidth: 1024, maxHeight: 1024, maxSize: 5 * 1024 * 1024/* 5 MB*/, cropImage: true } 
     ]
 });
 
+app.post('/api/upload/music-album', upload.fields([{ name: 'albumCover', maxCount: 1 }, { name: 'songs', maxCount: 10 }]), async (req, res) => {
+    const request = new Request(req, res);
 
-async function postAlbums(req, res) {
-    try {
-        const filesToValidate = {
-            albumCover: req.files['albumCover'] ? [req.files['albumCover'][0]] : [],
-            songs: req.files['songs'] || []
-        };
-        await albumUploader.validate(filesToValidate);
-        res.json({ message: 'Music album uploaded successfully' });
-    } catch (error) {
-        console.error('Error uploading music album:', error);
-        res.status(400).send(error.message);
+    const requiredFields = ['album_title', 'username', 'created_at'];
+    const validFields = request.validatePostFields(requiredFields);
+
+    if (!validFields) {
+        return request.respond400MissingPostFields();
     }
-}
+    
+    const filesToValidate = {
+        albumCover: req.files['albumCover'] ? [req.files['albumCover'][0]] : [],
+        songs: req.files['songs'] || []
+    };
+
+    const validation = await albumUploader.validate(filesToValidate);
+    if (Array.isArray(validation) && validation.length > 0) {
+        return res.status(400).json({ errors: validation });
+    }
+
+    const created_at = new Date(validFields.created_at);
+    const year = created_at.getFullYear();
+    const sanitizedAlbumTitle = validFields.album_title.replace(/\W+/g, '_');
+    const albumFolder = `${__dirname}/albums/${validFields.username}-${sanitizedAlbumTitle}-${year}/`;
+
+
+    if (!fs.existsSync(albumFolder)) {
+        fs.mkdirSync(albumFolder, { recursive: true });
+    }
+
+    if (req.files['albumCover']) {
+        const coverFile = req.files['albumCover'][0];
+        const coverFileName = `${sanitizedAlbumTitle}_cover${path.extname(coverFile.originalname)}`;
+        const coverFilePath = path.join(albumFolder, coverFileName);
+        fs.writeFileSync(coverFilePath, coverFile.buffer);
+    }
+
+    if (req.files['songs']) {
+        const songs = req.files['songs'];
+        songs.forEach((songFile, index) => {
+            const songFileName = `song_${index + 1}${path.extname(songFile.originalname)}`;
+            const songFilePath = path.join(albumFolder, songFileName);
+            fs.writeFileSync(songFilePath, songFile.buffer);
+        });
+    }
+
+    request.respond200Success({ message: 'Music album uploaded successfully' });
+});
+
+
+const albumUploader = new FileUploader({
+    fields: [
+        { name: 'albumCover', type: 'image', allowedTypes: ['image/jpeg', 'image/png'], maxWidth: 1024, maxHeight: 1024, maxSize: 5 * 1024 * 1024/* 5 MB*/, cropImage: false }, 
+        { name: 'songs', type: 'audio', allowedTypes: ['audio/mpeg', 'audio/wav'], maxDuration: 300, maxSize: 50 * 1024 * 1024 /* 50 MB*/}
+    ]
+});
